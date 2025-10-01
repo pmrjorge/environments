b1e4cd89c16dad4247dc402d988c45d76e43c9d5
Pedro Jorge <pmrjge@gmail.com>
2025-10-01 21:03:10 +0100

    Fixed pytorch containers image

--- pytorch_llms/install.sh
+++ pytorch_llms/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 conda install -n engine conda-forge::datasets conda-forge::peft conda-forge::transformers conda-forge::bitsandbytes conda-forge::jupyterlab -y
 conda install -n engine conda-forge::accelerate conda-forge::triton -y
-conda install -n engine conda-forge::pytorch
+conda run -n engine pip install torch torchvision --index-url https://download.pytorch.org/whl/cu129
