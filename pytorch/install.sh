06d08850a6f0a8c56bc30059fad034d6a7274a7d
Pedro Jorge <pmrjge@gmail.com>
2025-11-07 19:11:09 +0000

    Simplified image for pytorch

--- pytorch/install.sh
+++ pytorch/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-conda install -n engine conda-forge::datasets conda-forge::peft conda-forge::transformers conda-forge::bitsandbytes conda-forge::jupyterlab -y
-conda install -n engine conda-forge::accelerate conda-forge::triton -y
+conda run -n engine pip install jupyterlab matplotlib
+conda run -n engine pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130
+
+
+
