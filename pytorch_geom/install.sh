542ed6a068690166baf2d2e1169a6b963fa80ad8
Pedro Jorge <pmrjge@gmail.com>
2025-11-09 19:26:00 +0000

    Added pytorch geometric support

---
+++ pytorch_geom/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+conda run -n engine pip install jupyterlab matplotlib
+conda run -n engine pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130
+conda run -n engine pip install torch_geometric
+
