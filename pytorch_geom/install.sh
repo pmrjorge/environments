07303db33168f761cec95ecd8cc8b6b9fb610f73
Pedro Jorge <pmrjge@gmail.com>
2025-11-10 21:49:24 +0000

    Fixed dependency for pytorch geometric

--- pytorch_geom/install.sh
+++ pytorch_geom/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 conda run -n engine pip install jupyterlab matplotlib
-conda run -n engine pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.8.0+cu129.html
+conda run -n engine pip install torch_geometric pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.8.0+cu129.html
 
